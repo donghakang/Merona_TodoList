780ce048a9985b27d2c6bc2069597421abe773d7
Dongha Kang <dkang0602@gmail.com>
2020-12-31 01:48:29 +0900

    added Bottom Navigation Bar

--- WhenYouComeMerona/app/src/main/java/com/example/whenyoucomemerona/MainActivity.java
+++ WhenYouComeMerona/app/src/main/java/com/example/whenyoucomemerona/MainActivity.java
@@ -5,16 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, com.example.whenyoucomemerona.LoginActivity.class);
-        startActivity(intent);
-
-        finish();
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setItemIconTintList(null);
     }
 }
