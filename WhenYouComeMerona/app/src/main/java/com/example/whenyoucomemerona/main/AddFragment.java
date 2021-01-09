6d8d0d6103dfc37605dddfca39b1add3e4717e00
Dongha Kang <dkang0602@gmail.com>
2021-01-09 20:32:11 +0900

    add page

--- WhenYouComeMerona/app/src/main/java/com/example/whenyoucomemerona/main/AddFragment.java
+++ WhenYouComeMerona/app/src/main/java/com/example/whenyoucomemerona/main/AddFragment.java
@@ -2,12 +2,15 @@ package com.example.whenyoucomemerona.main;
 
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.example.whenyoucomemerona.R;
 import com.example.whenyoucomemerona.controller.BaseFragment;
@@ -16,9 +19,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class AddFragment extends BaseFragment {
+public class AddFragment extends BaseFragment implements View.OnClickListener {
 
     EditText etContent;
+    EditText etMemo;
+
+    EditText etDate, etTime, etLocation, etShare;
+    SwitchCompat switchDate, switchTime, switchLocation, switchShare;
+
     Button   btnSubmit;
 
     public AddFragment() {
@@ -36,15 +44,28 @@ public class AddFragment extends BaseFragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add, container, false);
         etContent = view.findViewById(R.id.et_content);
+        etMemo = view.findViewById(R.id.et_memo);
+        etDate = view.findViewById(R.id.et_date);
+        etTime = view.findViewById(R.id.et_time);
+        etLocation = view.findViewById(R.id.et_location);
+        etShare = view.findViewById(R.id.et_share);
+        switchDate = view.findViewById(R.id.switch_date);
+        switchTime = view.findViewById(R.id.switch_time);
+        switchLocation = view.findViewById(R.id.switch_location);
+        switchShare = view.findViewById(R.id.switch_share);
+
         btnSubmit = view.findViewById(R.id.btn_submit);
 
+        etDate.setOnClickListener(this);
+        etTime.setOnClickListener(this);
+        etLocation.setOnClickListener(this);
+        etShare.setOnClickListener(this);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 insertItem();
             }
         });
-
         return view;
     }
 
@@ -73,4 +94,18 @@ public class AddFragment extends BaseFragment {
         }
     }
 
+    // EDIT TEXT 가 눌러질 경우
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.et_date) {
+            // 날짜
+            Log.d("debugging", "날짜 선택");
+        } else if (v.getId() == R.id.et_time) {
+            Log.d("debugging", "시간 선택");
+        } else if (v.getId() == R.id.et_location) {
+            Log.d("debugging", "장소 선택");
+        } else if (v.getId() == R.id.et_share) {
+            Log.d("debugging", "공유 선택");
+        }
+    }
 }
