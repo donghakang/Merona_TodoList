5b04b891b66b3fdd7a0567696fb16adcf3e80dc1
Dongha Kang <50029346+donghakang@users.noreply.github.com>
2021-01-11 23:37:07 +0900

    Update URL.java

--- WhenYouComeMerona/app/src/main/java/com/example/whenyoucomemerona/model/URL.java
+++ WhenYouComeMerona/app/src/main/java/com/example/whenyoucomemerona/model/URL.java
@@ -3,7 +3,7 @@ package com.example.whenyoucomemerona.model;
 public class URL {
 
     public static String getUrl() {
-        String url = "http://172.30.1.12:8095/merona/";
+
         return url;
     }
 }
